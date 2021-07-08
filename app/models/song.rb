aee6f5d3203da3f24a02f5a00e1464a50285c379
SamHarron <harronsam@gmail.com>
2021-07-07 21:44:14 -0400

    commit tree a99e95d3085bf5a5e64909ba109fec606ac9c4a8

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,12 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
 
-  def artist_name
+  def artist_name   #reader - gets the artist name
+    artist.name if artist
   end
 
-  def artist_name=(name)
+  def artist_name=(name)  #writer - sets the artist name if no song artist name is found
+    a = Artist.find_or_create_by(name: name)
+    self.artist = a
   end
 end
